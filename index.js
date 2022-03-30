d7312b36041739f5ec1b1783af30777ba5bc7431
Baska Batbold <baska.batbold@kin.com>
2022-03-29 22:12:33 -0500

    test 1

--- index.js
+++ index.js
@@ -2,70 +2,99 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { graphql } = require("@octokit/graphql");
 
-const graphqlWithAuth = graphql.defaults({
-  headers: {
-    authorization: core.getInput('access-token'),
-  },
-});
+const run = async () => {
+  const payload = github.context.payload;
+  const pullRequest = payload.pull_request;
+  const targetProjectId = core.getInput("target-project");
+  const orgName = payload.organization.login;
+  const repositoryName = payload.repository.name;
+  const accessToken = core.getInput("access-token");
 
-const { issues } = await graphqlWithAuth(
-  `
-    {
-      node(id: "PN_kwDOAVkpBs4ABEih") {
-        ... on ProjectNext {
-          items(first: 10, after: null) {
-            pageInfo {
-              hasNextPage
-              endCursor
-            }
-            nodes {
-              title
-              content {
-                ... on Issue {
-                  id
-                  number
-                  url
-                  projectCards {
-                    edges {
-                      node {
-                        id
-                        column {
-                          name
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
+  console.log(payload);
+  console.log(pullRequest.number, "pully number");
+  console.log(orgName, "org name");
+  console.log(repositoryName, "repo");
+  getIssuesFromPullRequest(
+    pullRequest.number,
+    orgName,
+    repositoryName,
+    accessToken
+  )
+    .then((data) => {
+      console.dir(data.repository.pullRequest.closingIssuesReferences);
+      console.log(
+        data.repository.pullRequest.closingIssuesReferences.nodes.length +
+          " - this many nodes"
+      );
+      data.repository.pullRequest.closingIssuesReferences.nodes.map(({ id }) =>
+        addIssuesToProject(targetProjectId, id)
+      );
+    })
+    .catch((e) => {
+      core.setFailed(e.message);
+    });
+};
+
+async function addIssuesToProject(issueNodeIds, projectId) {
+  issueNodeIds.map((nodeId) =>
+    addIssueToProject(projectId, nodeId, core.getInput("access-token"))
+  );
+}
+
+// Requests
+
+async function addIssueToProject(projectId, contentId, accessToken) {
+  return graphql(addIssueToProjectMutation, {
+    projectId: projectId,
+    contentId: contentId,
+    headers: {
+      authorization: `bearer ${accessToken}`,
+    },
+  });
+}
+
+async function getIssuesFromPullRequest(
+  pullRequestNumber,
+  owner,
+  repository,
+  accessToken
+) {
+  return graphql(getResolvingIssueQuery, {
+    owner: owner,
+    repository: repository,
+    number: pullRequestNumber,
+    headers: {
+      authorization: `bearer ${accessToken}`,
+    },
+  });
+}
+
+// Queries & Mutations
+
+const getResolvingIssueQuery = `
+  query GetResolvingIssue($owner: String!, $repository: String!, $number: Int!) {
+    repository(owner:$owner, name:$repository) {
+      pullRequest(number: $number) {
+        id
+        title
+        closingIssuesReferences(last: 10) {
+          nodes {
+            id
           }
         }
       }
     }
-  `
-);
+  }
+`;
 
-const addIssueToProject = await graphqlWithAuth(
-  `mutation {
-    addProjectNextItem(input: {projectId: "MDExOlByb2plY3ROZXh0MzQ5MA==" contentId: "I_kwDOBCvUzs5Fw8KB"}) {
+const addIssueToProjectMutation = `
+  mutation AddIssueToProject($projectId: ID!, $contentId: ID!) {
+    addProjectNextItem(input: {projectId: $projectId, contentId: $contentId}) {
       projectNextItem {
         id
       }
     }
   }
-`
-)
-try {
-  const run = async () => {
-    try {
-      console.log(core.getInput('project-to-add-to'))
-      console.log(issues)
-    } catch (error) {
-      core.info(error);
-    }
-  };
+`;
 
-  run();
-} catch (error) {
-  core.setFailed(error.message);
-};
+run();
